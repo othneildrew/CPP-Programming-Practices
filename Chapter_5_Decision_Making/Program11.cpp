b96044a4a878d93cabd62b65ff8dc6c39af716c6
Othneil Drew <codeguydrew@gmail.com>
2019-03-05 09:21:50 -0600

    Create Program11.cpp

---
+++ Chapter_5_Decision_Making/Program11.cpp
@@ -0,0 +1,18 @@
+// Chapter 5: Program 11
+
+/***
+Referring to the truth table of logical operators, for the following segment
+of code:
+**/ 
+
+if (Input1_1 && Input2)
+    cout << " output is TRUE";
+else ()
+    cout << " output is FALSE";
+
+// Explanation: Is it AND or OR? Why?
+// Returns TRUE. The AND operator returns true if all operantds are true;
+// otherwise it retruns false. Assuming that same value is assigned to values
+// in test condition, the statement returns true.
+
+// Code written by: Othneil Drew
