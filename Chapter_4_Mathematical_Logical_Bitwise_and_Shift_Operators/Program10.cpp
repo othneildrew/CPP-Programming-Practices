11f7a68b07c4dc10a13e836b5281524cc998e935
Othneil Drew <codeguydrew@gmail.com>
2019-03-04 23:42:37 -0600

    Create Program10.cpp

---
+++ Chapter_4_Mathematical_Logical_Bitwise_and_Shift_Operators/Program10.cpp
@@ -0,0 +1,30 @@
+// Chapter 4: Program 9
+
+/***
+Write a C++ program to read a number as type of short integer. Shift the number
+3 bits to the left, and display the output.
+**/ 
+
+# include <iostream>
+# include <math.h>
+# include <string>
+using namespace std;
+
+int main(void)
+{
+    short Number, Shifted;
+    
+    cout << "Please enter a number \n";
+    cin >> Number;
+    
+    cout << "Number entered = " << Number << endl;
+    
+    // Shift number 3 bits to the left
+    Shifted = Number << 3;
+    cout << "Number, shifted 3 bits left = " << Shifted << endl;;
+    
+    system("pause");
+    return 0;
+}
+
+// Code written by: Othneil Drew
