f0de3b2763ef5fbeacdbb6ad618bc2bae3d86979
Othneil Drew <codeguydrew@gmail.com>
2019-05-08 17:26:53 -0500

    Update Program5.cpp

--- Exam_3/Program5.cpp
+++ Exam_3/Program5.cpp
@@ -7,24 +7,47 @@
 # include <iostream>
 using namespace std;
 
+int Read_Data(void), Compute_Factorial(int);
+void Display_Data(int);
+
+int Factorial = 0;
+
 int main(void) {
 
-	int Number, Factorial = 0, Size = 1;
+	int Number;
 
 	cout << "\n\t This program finds the factorial of a number \n";
 	cout << "\n\t To get started, please enter an integer number \n";
-	cin >> Number;
+	Number = Read_Data();
 	Factorial = Number;
+	
+	Factorial = Compute_Factorial(Number);
 
-	while (Size != Number) {
-		Factorial *= Size;
-		Size++;
-	}
-
-	cout << Number << "! = " << Factorial << endl;
+	cout << Number << "! = ";
+	Display_Data(Factorial);
 
 	system("pause");
 	return 0;
 }
 
 // Code written by: Othneil Drew
+
+int Read_Data() {
+	int data;
+	cin >> data;
+	return data;
+}
+
+void Display_Data(int data) {
+	cout << data << endl;
+}
+
+int Compute_Factorial(int number) {
+	int Size = 1;
+
+	while (Size != number) {
+		Factorial *= Size;
+		Size++;
+	}
+	return Factorial;
+}
